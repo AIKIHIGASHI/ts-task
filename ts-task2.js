4a2ec73812b0a2a77360739b6a80e332e0f8471a
AIKIHIGASHI <sheltie0805@gmail.com>
2021-02-16 19:37:09 +0900

    クラスの型付け

--- ts-task2.js
+++ ts-task2.js
@@ -3,28 +3,55 @@ class ObjectWrapper {
     /***
      * 引数のオブジェクトのコピーを this._objに設定
      */
-    constructor(_obj) { }
+    constructor(_obj) {
+        this._obj = _obj;
+    }
     /**
      * this._objのコピーを返却
      * @return Object
      */
-    get obj() { }
+    get obj() {
+        return this._obj;
+    }
     /**
      * this._obj[key] に valを設定。keyがthis._objに存在しない場合、falseを返却
      * @param key オブジェクトのキー
      * @param val オブジェクトの値
      */
-    set(key, val) { }
+    set(key, val) {
+        if (key in this._obj) {
+            this._obj[key] = val;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
     /**
      * 指定したキーの値のコピーを返却
      * 指定のキーが存在しない場合 undefinedを返却
      * @param key オブジェクトのキー
      */
-    get(key) { }
+    get(key) {
+        if (key in this._obj) {
+            return this._obj[key];
+        }
+        else {
+            return undefined;
+        }
+    }
     /**
      * 指定した値を持つkeyの配列を返却。該当のものがなければ空の配列を返却。
      */
-    findKeys(val) { }
+    findKeys(val) {
+        const keys = [];
+        for (const key in this._obj) {
+            if (this._obj[key] === val) {
+                keys.push(key);
+            }
+        }
+        return keys;
+    }
 }
 /**
  * check script
@@ -38,9 +65,7 @@ if (wrappedObj1.obj.a === '01') {
 else {
     console.error('NG: get obj()');
 }
-if (wrappedObj1.set('c', '03') === false &&
-    wrappedObj1.set('b', '04') === true &&
-    wrappedObj1.obj.b === '04') {
+if (wrappedObj1.set('c', '03') === false && wrappedObj1.set('b', '04') === true && wrappedObj1.obj.b === '04') {
     console.log('OK: set(key, val)');
 }
 else {
@@ -55,11 +80,7 @@ else {
 const obj2 = { a: '01', b: '02', bb: '02', bbb: '02' };
 const wrappedObj2 = new ObjectWrapper(obj2);
 const keys = wrappedObj2.findKeys('02');
-if (wrappedObj2.findKeys('03').length === 0 &&
-    keys.includes('b') &&
-    keys.includes('bb') &&
-    keys.includes('bbb') &&
-    keys.length === 3) {
+if (wrappedObj2.findKeys('03').length === 0 && keys.includes('b') && keys.includes('bb') && keys.includes('bbb') && keys.length === 3) {
     console.log('OK: findKeys(val)');
 }
 else {
--- ts-task2.ts
+++ ts-task2.ts
@@ -1,35 +1,64 @@
+interface StringKeyObject {
+  [key: string]: string;
+}
+
 class ObjectWrapper {
-  private _obj;
+  private _obj: StringKeyObject;
 
   /***
    * 引数のオブジェクトのコピーを this._objに設定
    */
-  constructor(_obj: Object) {}
+  constructor(_obj: StringKeyObject) {
+    this._obj = _obj;
+  }
 
   /**
    * this._objのコピーを返却
    * @return Object
    */
-  get obj() {}
+  get obj(): StringKeyObject {
+    return this._obj;
+  }
 
   /**
    * this._obj[key] に valを設定。keyがthis._objに存在しない場合、falseを返却
    * @param key オブジェクトのキー
    * @param val オブジェクトの値
    */
-  set(key, val): boolean {}
+  set(key: string, val: string): boolean {
+    if (key in this._obj) {
+      this._obj[key] = val;
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   /**
    * 指定したキーの値のコピーを返却
    * 指定のキーが存在しない場合 undefinedを返却
    * @param key オブジェクトのキー
    */
-  get(key) {}
+  get(key: string): string | undefined {
+    if (key in this._obj) {
+      return this._obj[key];
+    } else {
+      return undefined;
+    }
+  }
 
   /**
    * 指定した値を持つkeyの配列を返却。該当のものがなければ空の配列を返却。
    */
-  findKeys(val: unknown) {}
+  findKeys(val: string): string[] {
+    const keys = [];
+    for (const key in this._obj) {
+      if (this._obj[key] === val) {
+        keys.push(key);
+      }
+    }
+    return keys;
+  }
 }
 
 /**
@@ -45,11 +74,7 @@ if (wrappedObj1.obj.a === '01') {
   console.error('NG: get obj()');
 }
 
-if (
-  wrappedObj1.set('c', '03') === false &&
-  wrappedObj1.set('b', '04') === true &&
-  wrappedObj1.obj.b === '04'
-) {
+if (wrappedObj1.set('c', '03') === false && wrappedObj1.set('b', '04') === true && wrappedObj1.obj.b === '04') {
   console.log('OK: set(key, val)');
 } else {
   console.error('NG: set(key, val)');
@@ -64,13 +89,7 @@ if (wrappedObj1.get('b') === '04' && wrappedObj1.get('c') === undefined) {
 const obj2 = { a: '01', b: '02', bb: '02', bbb: '02' };
 const wrappedObj2 = new ObjectWrapper(obj2);
 const keys = wrappedObj2.findKeys('02');
-if (
-  wrappedObj2.findKeys('03').length === 0 &&
-  keys.includes('b') &&
-  keys.includes('bb') &&
-  keys.includes('bbb') &&
-  keys.length === 3
-) {
+if (wrappedObj2.findKeys('03').length === 0 && keys.includes('b') && keys.includes('bb') && keys.includes('bbb') && keys.length === 3) {
   console.log('OK: findKeys(val)');
 } else {
   console.error('NG: findKeys(val)');
